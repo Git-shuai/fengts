29cc330a071403a538c28cc7cb7c1d23e2035898
tian <1873461847@qq.com>
2020-11-30 14:52:09 +0800

    项目初始化

---
+++ fengts-service/src/main/java/tian/web/FengtsApplication.java
@@ -0,0 +1,17 @@
+package tian.web;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author tian
+ * @date 2020/11/30
+ */
+@SpringBootApplication
+public class FengtsApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(FengtsApplication.class, args);
+    }
+
+}
